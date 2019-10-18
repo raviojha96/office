2833ffee54e2a5653918863a8bc9a28c7abefb22
Ravi Ojha <raviojha500@gmail.com>
2019-10-18 16:30:03 +0530

    validity

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render(){
+        return (
+        <footer>
+            <div>
+                Pebibits Technologies Pvt. Ltd. All Right Reserved | Design by <a href="">Pebibits Technologies</a>
+            </div>
+        </footer>
+        );
+        }
+    }
+export default Footer;
